c99118b521dad807dbd51f0cc72d27e6cb10cda6
woodgoal <woodgoal2000@yahoo.fr>
2011-08-02 11:47:51 +0200

    -Ajout de la library

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -4,6 +4,10 @@ class ErrorController extends Zend_Controller_Action
 {
     public function errorAction() 
     {
-        
+         $errorHandler = $this->_getParam('error_handler');    
+         $exception = $errorHandler->exception; 
+         
+         $this->view->message = $exception->getMessage(); 
+         $this->view->code = $exception->getCode(); 
     }   
 }
---
+++ library/Ip/View/Helper/FormatDate.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Ip
+ * @package    Ip_View
+ * @subpackage Helper
+ */
+
+/** @category   Ip
+  * @package    Ip_View
+  * @subpackage Helper
+  */
+
+class Ip_View_Helper_FormatDate extends Zend_View_Helper_Abstract {
+    
+    public function formatDate($date)
+    {
+        return 'Jeudi 24 Novembre';
+    }
+}    
